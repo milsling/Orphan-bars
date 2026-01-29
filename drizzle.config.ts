0218ff3146bc0a03dbe5a8dab1aecfd255c80c20
milsling <picconetrevor@gmail.com>
2026-01-29 17:10:47 +0000

    commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -17,5 +17,3 @@ export default defineConfig({
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql);
