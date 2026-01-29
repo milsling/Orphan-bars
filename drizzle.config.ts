9ac194acb64949a05231774f8019dc60bd29921d
milsling <picconetrevor@gmail.com>
2026-01-29 05:54:19 +0000

    commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -12,3 +12,10 @@ export default defineConfig({
     url: process.env.DATABASE_URL,
   },
 });
+
+// Before (and still works after update)
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
